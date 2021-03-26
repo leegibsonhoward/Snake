148768abf093d486e2cfc310e7b2bb1517dd5d83
Lee Howard <leegibsonhoward@gmail.com>
2021-03-25 19:20:48 -0700

    Initial commit
    - Add base project files

---
+++ src/main.c
@@ -0,0 +1,47 @@
+#include <allegro.h>
+
+#define MODE GFX_AUTODETECT_WINDOWED
+
+int main(void)
+{
+    /* you should always do this at the start of Allegro programs */
+    if (allegro_init() != 0)
+        return 1;
+
+    /* set up the keyboard handler */
+    install_keyboard();
+
+    /* set a graphics mode sized 320×200 */
+    if (set_gfx_mode(MODE, 640, 480, 0, 0) != 0)
+    {
+        if (set_gfx_mode(GFX_SAFE, 640, 480, 0, 0) != 0)
+        {
+            set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+            allegro_message("Unable to set any graphic mode\n %s\n", allegro_error);
+            return 1;
+        }
+    }
+
+    /* set the color palette */
+    set_palette(desktop_palette);
+    /* clear the screen to white */
+    clear_to_color(screen, makecol(255, 255, 255));
+
+    /* you don’t need to do this, but on some platforms (eg. Windows) things
+* will be drawn more quickly if you always acquire the screen before
+* trying to draw onto it.
+*/
+    acquire_screen();
+
+    /* write some text to the screen with black letters and transparent background */
+    textout_centre_ex(screen, font, "Snake Game", SCREEN_W / 2, SCREEN_H / 2, makecol(0, 0, 0), -1);
+
+    /* you must always release bitmaps before calling any input functions */
+    release_screen();
+
+    /* wait for a keypress */
+    readkey();
+
+    return 0;
+}
+END_OF_MAIN()
